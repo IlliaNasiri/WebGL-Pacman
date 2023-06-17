e6f1a1425b950dcab96d64ffdd839c8f83beaf78
Ilya Nasiri <46106844+LogoStick@users.noreply.github.com>
2023-06-16 21:22:47 -0700

    Have implemented getLegalMovesFunction for GameEntity, that would allow Pacman and Ghosts not to crash into a wall or go out of borders.
    Added a few classes that would be underlying the game logic.

--- Pacman.js
+++ Pacman.js
@@ -1,21 +1,132 @@
 
-class Game {
+class PositionHelper {
+    //TODO: CHECK COMPATIBILITY WITH OTHER BROWSERS FOR STATIC KEYWORD
+    static positionEqual(pos1, pos2) {
+        return pos1[0] == pos2[0] && pos1[1] == pos2[1];
+    }
+    static add(position, direction) {
+        return [ position[0] + direction[0], position[1] + direction[1] ]
+    }
+    static subtract(position, direction) {
+        return [position[0] - direction[0], position[1] - direction[1]]
+    }
+}
 
-    constructor() {
+class Game {
+    LOGICAL_WIDTH = 9;
+    LOGICAL_HEIGHT = 10;
+    constructor(walls, dots) {
+        this.score = 0;
+        this.timer = 60;
+        this.walls = walls;
+        this.dots = dots;
     }
 
 }
 
-class PacmanEntity {}
+class WallEntity {
+    constructor(positions, renderCoordinates) {
+        this.positions = positions;
+        this.renderCoordinates = renderCoordinates;
+    }
 
-class GhostEntity {}
+}
+
+class GameEntity {
+    //TODO: ADD MOVEMENT DIRECTION
+    constructor(x0, y0, renderCoordinates, walls, LOGICAL_WIDTH, LOGICAL_HEIGHT) {
+        this.position = [x0, y0];
+        this.renderCoordinates = renderCoordinates;
+        this.walls = walls;
+        this.LOGICAL_WIDTH = LOGICAL_WIDTH;
+        this.LOGICAL_HEIGHT = LOGICAL_HEIGHT;
+        this.actions = [
+            [0, 1], // North
+            [0, -1], // South
+            [1, 0], // East,
+            [-1, 0] // West
+        ]
+    }
+    getLegalMoves() {
+        let legalMoves = []
+        this.actions.forEach( action => {
+            let newPosition = PositionHelper.add(this.position, action)
+            if(!this.doesPositionOverlapWithWalls(newPosition) && !this.doesExitGameBorders(newPosition)) {
+                legalMoves.push(action);
+            }
+        } )
+        return legalMoves;
+    }
+
+    doesPositionOverlapWithWalls(position) {
+
+        for(let WallEntity of this.walls) {
+            for(let wallPosition of WallEntity.positions) {
+                if( PositionHelper.positionEqual(wallPosition, position) ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    doesExitGameBorders(position) {
+        return (position[0] < 0 || position[0] >= this.LOGICAL_WIDTH) || (position[1] < 0 || position[1] >= this.LOGICAL_HEIGHT)
+    }
+
+}
+
+class PacmanEntity extends GameEntity{
+    constructor(x0, y0, renderCoordinates) {
+        super(x0, y0, renderCoordinates)
+    }
+
+}
+
+class GhostEntity extends GameEntity{
+    constructor(x0, y0, renderCoordinates) {
+        super(x0, y0, renderCoordinates)
+    }
+
+}
 
 // The main loop of the game that keeps the game running
-function main() {}
+
+const walls = [
+    new WallEntity(
+        [ [1,1], [2,1], [3,1], [1,2], [2,2], [3,2] ],
+        []
+    ),
+    new WallEntity(
+        [ [5,1], [6,1], [7,1], [5,2], [6,2], [7,2] ],
+        []
+    ),
+    new WallEntity(
+        [ [1, 4], [1, 5]],
+        []
+    ),
+    new WallEntity(
+        [[7,4], [7,5]],
+        []
+    ),
+    new WallEntity(
+        [ [1,7], [2,7], [3,7], [1,8], [2,8], [3,8] ],
+        []
+    ),
+    new WallEntity(
+        [ [5, 7], [6, 7], [7,7], [5,8], [6,8], [7,8]],
+        []
+    )
+]
+
+function main() {
+
+    let gameEntity = new GameEntity(1,0, [], walls, 9, 10);
+
+
+}
 
 main();
 
 // For testing purposes, export needed classes and functions
-export {
-    main
-}
+export {PositionHelper}
--- Test/Test.js
+++ Test/Test.js
@@ -1,6 +1,31 @@
-import {main} from '../Pacman.js'
+import {PositionHelper} from '../Pacman.js'
 
 const assert = chai.assert;
-describe("Fist Test", function() {})
+describe("PositionHelper.equal()", function() {
+    it("pos1 = [1,1], pos2 = [1,1], Expected: true", function () {
+        assert.equal(true, PositionHelper.positionEqual([1,1], [1,1]));
+    })
+    it("pos1 = [1,2], pos2 = [1,1], Expected: false", function () {
+        assert.notEqual(true, PositionHelper.positionEqual([1,2], [1,1]))
+    })
+    it("pos1 = [], pos2 = [1,2], Expected: false", function () {
+        assert.notEqual(true, PositionHelper.positionEqual([], [1,2]))
+    })
+});
+
+describe("PositionHelper.add()", function () {
+    it("pos1 = [1,1], pos2 = [0,2], expected = [1,3]", function () {
+        assert.equal(1, PositionHelper.add([1,1], [0,2])[0])
+        assert.equal(3, PositionHelper.add([1,1], [0,2])[1])
+    })
+})
+
+describe("PositionHelper.subtract()", function () {
+    it("pos1 = [1,1], pos2 = [0,2], expected = [1,-1]", function () {
+        assert.equal(1, PositionHelper.subtract([1,1], [0,2])[0])
+        assert.equal(-1, PositionHelper.subtract([1,1], [0,2])[1])
+    })
+})
+
 
 mocha.run();
