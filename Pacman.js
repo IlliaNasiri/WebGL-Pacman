aa84a54fbea214261cc39ea3642807b957696142
Ilya Nasiri <46106844+LogoStick@users.noreply.github.com>
2023-06-17 17:57:02 -0700

    Implemented ghost chase functionality (greedy and random, minimax is to be done)

--- Pacman.js
+++ Pacman.js
@@ -1,4 +1,4 @@
-
+"use strict"
 class PositionHelper {
     //TODO: CHECK COMPATIBILITY WITH OTHER BROWSERS FOR STATIC KEYWORD
     static positionEqual(pos1, pos2) {
@@ -10,7 +10,13 @@ class PositionHelper {
     static subtract(position, direction) {
         return [position[0] - direction[0], position[1] - direction[1]]
     }
+
+    static manhattanDistance(pos1, pos2) {
+        return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
+    }
 }
+
+class GameManager {}
 
 class Game {
     LOGICAL_WIDTH = 9;
@@ -107,14 +113,54 @@ class PacmanEntity extends GameEntity{
 }
 
 class GhostEntity extends GameEntity{
-    constructor(x0, y0, renderCoordinates, walls, LOGICAL_WIDTH, LOGICAL_HEIGHT) {
-        super(x0, y0, renderCoordinates, walls, LOGICAL_WIDTH, LOGICAL_HEIGHT)
+
+    constructor(x0, y0, renderCoordinates, walls, LOGICAL_WIDTH, LOGICAL_HEIGHT, ghostType) {
+        super(x0, y0, renderCoordinates, walls, LOGICAL_WIDTH, LOGICAL_HEIGHT);
+        // can be either greedy or minimax, or random
+        this.ghostType = ghostType;
     }
 
     getMoveAction() {
         // TODO: IMPLEMENT CHASING ALGORITHM FOR A GHOST
-        return [1,0]
+        let action = [0,0]
+
+        switch (this.ghostType) {
+            case "greedy":
+                //todo: pass pacman position
+                action = getGreedyAction();
+                break;
+            case "minimax":
+                action = getMinimaxAction();
+                break;
+            case "random":
+                action = getRandomAction();
+                break;
+        }
+        //TODO: MAKE EXCEPTION CLASS FOR EMPTY ACTION
+        return action;
     }
+
+    getGreedyAction(pacmanPosition) {
+        // EDGE CASE: PACMAN AND GHOST ARE IN SAME POSITION, RETURN [0,0]
+        if( PositionHelper.manhattanDistance(this.position, pacmanPosition) == 0 ) return [0,0];
+
+        let action = this.actions[0];
+        let manhattanDistance = PositionHelper.manhattanDistance( PositionHelper.add(this.position, action), pacmanPosition );
+        let legalMoves = this.getLegalMoves();
+        console.log(legalMoves)
+        for(let i = 1; i < legalMoves.length; i++) {
+            if(manhattanDistance >= PositionHelper.manhattanDistance( PositionHelper.add(this.position, legalMoves[i]), pacmanPosition )) {
+                action = legalMoves[i]
+                manhattanDistance = PositionHelper.manhattanDistance( PositionHelper.add(this.position, legalMoves[i]), pacmanPosition );
+            }
+        }
+        return action;
+    }
+
+    getRandomAction() {
+        return this.actions[Math.floor(Math.random() * 5)]
+    }
+
 
 }
 
@@ -148,7 +194,10 @@ const walls = [
 ]
 
 function main() {
-    let gameEntity = new GameEntity(1,0, [], walls, 9, 10);
+
+    let ghostEntity = new GhostEntity(0,7, [], walls, 9, 10, "greedy");
+    console.log(ghostEntity.getGreedyAction([2, 6]));
+
     // setInterval(() => {
     //     gameEntity.move();
     //     console.log(gameEntity.position)
@@ -162,7 +211,7 @@ function main() {
     // console.log(gameEntity.getLegalMoves())
     // gameEntity.moveDirection = [1,0]
     // gameEntity.move()
-    setUpDirectionKeyEventListener(gameEntity);
+    // setUpDirectionKeyEventListener(gameEntity);
 }
 
 function setUpDirectionKeyEventListener(pacman) {
